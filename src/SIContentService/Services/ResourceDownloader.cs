94493247ef70b997c036f22820cf50d4220f6c1c
VladimirKhil <vladimir.khil@gmail.com>
2024-02-21 20:25:45 +0100

    Add error logging to ResourceDownloader

--- src/SIContentService/Services/ResourceDownloader.cs
+++ src/SIContentService/Services/ResourceDownloader.cs
@@ -9,19 +9,30 @@ internal sealed class ResourceDownloader : IResourceDownloader
 {
     private readonly HttpClient _client;
     private readonly SIContentServiceOptions _options;
+    private readonly ILogger<ResourceDownloader> _logger;
 
     public ResourceDownloader(
         HttpClient client,
-        IOptions<SIContentServiceOptions> options)
+        IOptions<SIContentServiceOptions> options,
+        ILogger<ResourceDownloader> logger)
     {
         _client = client;
         _options = options.Value;
+        _logger = logger;
     }
 
     public async Task DownloadResourceAsync(Uri resourceUri, string targetPath, CancellationToken cancellationToken)
     {
-        using var stream = await _client.GetStreamAsync(resourceUri, cancellationToken);
-        using var targetStream = File.Create(targetPath, _options.BufferSize, FileOptions.Asynchronous);
-        await stream.CopyToAsync(targetStream, _options.BufferSize, cancellationToken);
+        try
+        {
+            using var stream = await _client.GetStreamAsync(resourceUri, cancellationToken);
+            using var targetStream = File.Create(targetPath, _options.BufferSize, FileOptions.Asynchronous);
+            await stream.CopyToAsync(targetStream, _options.BufferSize, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogWarning(ex, "Resource {resourceUri} -> {targetPath} download error: {error}", resourceUri, targetPath, ex.Message);
+            throw;
+        }
     }
 }
